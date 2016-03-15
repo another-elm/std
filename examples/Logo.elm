7d98344b9b00aeadc5229112fe61c450539babd6
Evan Czaplicki <info@elm-lang.org>
2016-03-14 17:34:09 -0700

    Update example for new version of things

--- examples/Logo.elm
+++ examples/Logo.elm
@@ -1,11 +1,10 @@
-import Svg (..)
-import Svg.Attributes (..)
-import Html (Html)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
-
-main : Html
 main =
-  svg [ version "1.1", x "0", y "0", viewBox "0 0 323.141 322.95" ]
+  svg
+    [ version "1.1", x "0", y "0", viewBox "0 0 323.141 322.95"
+    ]
     [ polygon [ fill "#F0AD00", points "161.649,152.782 231.514,82.916 91.783,82.916" ] []
     , polygon [ fill "#7FD13B", points "8.867,0 79.241,70.375 232.213,70.375 161.838,0" ] []
     , rect

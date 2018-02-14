e494d7305662c7e8243b608d6b8e152c77fad1fa
Evan Czaplicki <evancz@users.noreply.github.com>
2018-02-14 15:13:15 -0800

    Update for 0.19 / add higher lazyN / improve the Svg.Events API

--- src/Svg/Lazy.elm
+++ src/Svg/Lazy.elm
@@ -1,5 +1,5 @@
 module Svg.Lazy exposing
-  ( lazy, lazy2, lazy3
+  ( lazy, lazy2, lazy3, lazy4, lazy5, lazy6, lazy7, lazy8
   )
 
 {-| Since all Elm functions are pure we have a guarantee that the same input
@@ -14,7 +14,7 @@ If so, it skips calling the function!
 This is a really cheap test and often makes things a lot faster, but definitely
 benchmark to be sure!
 
-@docs lazy, lazy2, lazy3
+@docs lazy, lazy2, lazy3, lazy4, lazy5, lazy6, lazy7, lazy8
 -}
 
 import Svg exposing (Svg)
@@ -46,3 +46,38 @@ lazy2 =
 lazy3 : (a -> b -> c -> Svg msg) -> a -> b -> c -> Svg msg
 lazy3 =
   VirtualDom.lazy3
+
+
+{-| Same as `lazy` but checks on four arguments.
+-}
+lazy4 : (a -> b -> c -> d -> Svg msg) -> a -> b -> c -> d -> Svg msg
+lazy4 =
+  VirtualDom.lazy4
+
+
+{-| Same as `lazy` but checks on five arguments.
+-}
+lazy5 : (a -> b -> c -> d -> e -> Svg msg) -> a -> b -> c -> d -> e -> Svg msg
+lazy5 =
+  VirtualDom.lazy5
+
+
+{-| Same as `lazy` but checks on six arguments.
+-}
+lazy6 : (a -> b -> c -> d -> e -> f -> Svg msg) -> a -> b -> c -> d -> e -> f -> Svg msg
+lazy6 =
+  VirtualDom.lazy6
+
+
+{-| Same as `lazy` but checks on seven arguments.
+-}
+lazy7 : (a -> b -> c -> d -> e -> f -> g -> Svg msg) -> a -> b -> c -> d -> e -> f -> g -> Svg msg
+lazy7 =
+  VirtualDom.lazy7
+
+
+{-| Same as `lazy` but checks on eight arguments.
+-}
+lazy8 : (a -> b -> c -> d -> e -> f -> g -> h -> Svg msg) -> a -> b -> c -> d -> e -> f -> g -> h -> Svg msg
+lazy8 =
+  VirtualDom.lazy8

dbf29e35874118c4cf8443a7ea32a8339ad8f94d
Harry Sarson <harry.sarson@hotmail.co.uk>
2019-10-26 11:32:03 +0100

    implement List.mapN without kernel code

--- src/List.elm
+++ src/List.elm
@@ -429,27 +429,60 @@ If one list is longer, the extra elements are dropped.
 
 -}
 map2 : (a -> b -> result) -> List a -> List b -> List result
-map2 =
-  Elm.Kernel.List.map2
+map2 f xs1 xs2 =
+  reverse (map2Help f xs1 xs2 [])
 
 
 {-|-}
 map3 : (a -> b -> c -> result) -> List a -> List b -> List c -> List result
-map3 =
-  Elm.Kernel.List.map3
+map3 f xs1 xs2 xs3 =
+  reverse (map3Help f xs1 xs2 xs3 [])
 
 
 {-|-}
 map4 : (a -> b -> c -> d -> result) -> List a -> List b -> List c -> List d -> List result
-map4 =
-  Elm.Kernel.List.map4
+map4 f xs1 xs2 xs3 xs4 =
+  reverse (map4Help f xs1 xs2 xs3 xs4 [])
 
 
 {-|-}
 map5 : (a -> b -> c -> d -> e -> result) -> List a -> List b -> List c -> List d -> List e -> List result
-map5 =
-  Elm.Kernel.List.map5
+map5 f xs1 xs2 xs3 xs4 xs5 =
+  reverse (map5Help f xs1 xs2 xs3 xs4 xs5 [])
 
+
+map2Help :  (a -> b -> result) -> List a -> List b -> List result -> List result
+map2Help f xs1 xs2 ys =
+  case (xs1, xs2) of
+    (head1 :: rest1, head2 :: rest2) ->
+      map2Help f rest1 rest2 (cons (f head1 head2) ys)
+    _ ->
+      ys
+
+map3Help : (a -> b -> c -> result) -> List a -> List b -> List c-> List result -> List result
+map3Help f xs1 xs2 xs3 ys =
+  case (xs1, xs2, xs3) of
+    (head1 :: rest1, head2 :: rest2, head3 :: rest3) ->
+      map3Help f rest1 rest2 rest3 (cons (f head1 head2 head3) ys)
+    _ ->
+      ys
+
+map4Help : (a -> b -> c -> d -> result) -> List a -> List b -> List c -> List d -> List result -> List result
+map4Help f xs1 xs2 xs3 xs4 ys =
+  case (xs1, xs2, (xs3, xs4)) of
+    (head1 :: rest1, head2 :: rest2, (head3 :: rest3, head4 :: rest4)) ->
+      map4Help f rest1 rest2 rest3 rest4 (cons (f head1 head2 head3 head4) ys)
+    _ ->
+      ys
+
+
+map5Help : (a -> b -> c -> d -> e -> result) -> List a -> List b -> List c -> List d -> List e -> List result -> List result
+map5Help f xs1 xs2 xs3 xs4 xs5 ys =
+  case (xs1, xs2, (xs3, xs4, xs5)) of
+    (head1 :: rest1, head2 :: rest2, (head3 :: rest3, head4 :: rest4, head5 :: rest5)) ->
+      map5Help f rest1 rest2 rest3 rest4 rest5 (cons (f head1 head2 head3 head4 head5) ys)
+    _ ->
+      ys
 
 
 -- SORT

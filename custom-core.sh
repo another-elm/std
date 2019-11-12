013b47f6152a80a79174f92dae69fcf356e9969c
Harry Sarson <harry.sarson@hotmail.co.uk>
2019-11-12 21:53:39 +0000

    update custom-core script

--- custom-core.sh
+++ custom-core.sh
@@ -1,12 +1,12 @@
-
-if [[ ! -v ELM_HOME ]] || [[ $ELM_HOME != */custom-core ]]; then
-    export ELM_HOME="${ELM_HOME:-"$HOME/.elm"}/custom-core";
-fi
-
-bash <<"EOF"
+#! /usr/bin/env bash
 
 set -o errexit;
 set -o nounset;
+
+if [[ ! -v ELM_HOME ]]; then
+    eprintf "Please set ELM_HOME!"
+    exit 1
+fi
 
 printf "Sucess if ends with DONE: "
 
@@ -14,17 +14,21 @@ ELM="${ELM:-elm}"
 ELM_VERSION="$($ELM --version)"
 IFS=- read ELM_VERSION_START IGNORE <<< "$ELM_VERSION"
 
-rm -rf elm-minimal-master
-curl -sL https://github.com/harrysarson/elm-minimal/archive/master.tar.gz | tar xz
+rm -rf "$ELM_HOME/$ELM_VERSION/packages/elm/core/"
+
+if [[ ! -d elm-minimal-master ]]; then
+    curl -sL https://github.com/harrysarson/elm-minimal/archive/master.tar.gz | tar xz
+fi
+
 cd elm-minimal-master
+rm -rf elm-stuff
 $ELM make src/Main.elm --output /dev/null > /dev/null || true;
 cd - > /dev/null
-rm -rf elm-minimal-master
 
 
 CORE_VERSION="$(ls $ELM_HOME/$ELM_VERSION/packages/elm/core/)"
 CORE_PACKAGE_DIR="$ELM_HOME/$ELM_VERSION/packages/elm/core/$CORE_VERSION"
-CORE_GIT_DIR=$(pwd)
+CORE_GIT_DIR=$(realpath $1)
 
 rm -rf "$CORE_PACKAGE_DIR" > /dev/null
 ln -sv "$CORE_GIT_DIR" "$CORE_PACKAGE_DIR" > /dev/null
@@ -32,4 +36,3 @@ rm -vf "${CORE_GIT_DIR}"/*.dat "${CORE_GIT_DIR}"/doc*.json > /dev/null
 
 printf "DONE\n"
 
-EOF

f81ce3fc28c292ebe1e50e24ea0e5d3bfb89bab9
Harry Sarson <harry.sarson@hotmail.co.uk>
2019-12-22 21:17:16 +0000

    explain RawScheduler

--- src/Platform/RawScheduler.elm
+++ src/Platform/RawScheduler.elm
@@ -1,39 +1,23 @@
 module Platform.RawScheduler exposing (DoneCallback, ProcessId(..), Task(..), TryAbortAction, andThen, delay, kill, newProcessId, rawSend, rawSpawn, send, sleep, spawn)
 
-{-|
+{-| This module contains the low level logic for running tasks and processes. A
+`Task` is a sequence of actions (either syncronous or asyncronous) that will be
+run in order by the runtime. A process (outside this module a process is
+accessed and manipulated using its unique id) is a task paired with a
+"receiver". If a process is sent a message (using the `send` function) it is
+added to the processes mailbox. When the process completes execution of its
+current `Task` (or immediately if it has already finished execution of its
+`Task`) it will envoke its receiver function with the oldest message in the
+mailbox and the final state of its `Task`. The receiver function should produce
+a new `Task` for the process to execute.
 
+Processes spawned by user elm code (using `Process.spawn`) cannot receive
+messages so will execute their initial `Task` and then die.
 
-## Module notes:
-
-  - Types called `HiddenXXX` are used to bypass the elm type system.
-    The programmer takes **full responsibiliy** for making sure
-    that the types line up.
-    That does mean you have to second guess any and all strange
-    decisions I have made, hopefully things will get clearer over
-    time.
-
-  - The `Binding` constructor on the `Task` type is tricky one.
-      - It contains a callback function (that we will call `doEffect`)
-        and a `killer` function. `doEffect` will be called by
-        `Scheduler.enqueue` and will be passed another callback.
-        We call this second callback `doneCallback`.
-        `doEffect` should do its effects (which may be impure) and then,
-        when it is done, call `doneCallback`.`doEffect` **must** call
-        `doneCallback` and it **must** pass `doneCallback` a
-        `Task ErrX OkX` as an argument. (I am unsure about the values of
-        ErrX and OkX at the moment). The return value of `doEffect` may
-        be either `undefined` or a function that cancels the effect.
-      - If the second value `killer` is not Nothing, then the runtime
-        will call it if the execution of the `Task` should be aborted.
-
-
-## Differences between this and offical elm/core
-
-  - `Process.mailbox` is a (mutable) js array in elm/core and an elm list here.
-  - `Process.stack` is an (immutable) js linked list in elm/core and an elm list here.
-  - `Elm.Kernel.Scheduler.rawSend` mutates the process before enqueuing it in elm/core.
-    Here we create a **new** process with the **same** (unique) id and then enqueue it.
-    Same applies for (non-raw) `send`.
+Only two modules should import this module directly `Platform.Scheduler` and
+`Platform`. All other modules should import `Platform.Scheduler` which has a
+nicer API. `Platform` cannot import `Platform.Scheduler` as
+`Platfrom.Scheduler` imports `Platform` and elm does not allow import cycles.
 
 -}
 

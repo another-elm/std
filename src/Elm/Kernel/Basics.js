5f880d0be180d562f3bc28ffcac1fc4eb421fa18
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-01-02 23:45:49 +0000

    use modern js in basics.js

--- src/Elm/Kernel/Basics.js
+++ src/Elm/Kernel/Basics.js
@@ -6,31 +6,26 @@ import Elm.Kernel.Debug exposing (crash)
 
 // MATH
 
-var _Basics_pow = F2(Math.pow);
+const _Basics_pow = F2(Math.pow);
 
-var _Basics_cos = Math.cos;
-var _Basics_sin = Math.sin;
-var _Basics_tan = Math.tan;
-var _Basics_acos = Math.acos;
-var _Basics_asin = Math.asin;
-var _Basics_atan = Math.atan;
-var _Basics_atan2 = F2(Math.atan2);
+const _Basics_cos = Math.cos;
+const _Basics_sin = Math.sin;
+const _Basics_tan = Math.tan;
+const _Basics_acos = Math.acos;
+const _Basics_asin = Math.asin;
+const _Basics_atan = Math.atan;
+const _Basics_atan2 = F2(Math.atan2);
 
 
-var _Basics_ceiling = Math.ceil;
-var _Basics_floor = Math.floor;
-var _Basics_round = Math.round;
-var _Basics_sqrt = Math.sqrt;
-var _Basics_log = Math.log;
+const _Basics_ceiling = Math.ceil;
+const _Basics_floor = Math.floor;
+const _Basics_round = Math.round;
+const _Basics_sqrt = Math.sqrt;
+const _Basics_log = Math.log;
 
-var _Basics_modBy0 = function()
-{
-	__Debug_crash(11)
-};
+const _Basics_modBy0 = () => __Debug_crash(11);
 
-var _Basics_fudgeType = function(x) {
-	return x;
-};
+const _Basics_fudgeType = x => x;
 
 const _Basics_unwrapTypeWrapper__DEBUG = wrapped => {
 	const entries = Object.entries(wrapped);

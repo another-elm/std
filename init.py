b2eb12f260e24001155143a7108ec547134c6fb2
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-06-12 23:00:54 +0100

    nicer installation

--- init.py
+++ init.py
@@ -1,8 +1,12 @@
 #! /usr/bin/env python3
 
+import fileinput
 import os
+import sys
+from pathlib import Path
 
-elm_std_dir = os.path.dirname(os.path.realpath(__file__))
+elm_std_dir = Path(__file__).resolve().parent
+binary_path = Path.home() / ".local" / "bin" / "another-elm"
 
 
 def is_update_needed(package_root, relevant_custom_paths):
@@ -53,6 +57,28 @@ def reset_package(packages_root, author, package):
     return any_modified
 
 
+def install_exe(binary):
+    bin_dir = binary.parent
+
+    if bin_dir not in map(Path, os.getenv("PATH").split(":")):
+        print("WARNING: {} is not in PATH. Please add it!".format(bin_dir),
+              file=sys.stderr)
+
+    with os.fdopen(
+            os.open(binary,
+                    flags=os.O_CREAT | os.O_WRONLY | os.O_TRUNC,
+                    mode=0o777), "w") as f:
+
+        def print_to_file(s):
+            print(s, file=f, end='')
+
+        for line in fileinput.input(elm_std_dir / "another-elm"):
+            if line == "elm_std_dir = None  # REPLACE ME\n":
+                print_to_file('elm_std_dir = "{}"\n'.format(elm_std_dir))
+            else:
+                print_to_file(line)
+
+
 def main():
     elm_home_dir = os.getenv('ELM_HOME', default=os.path.expanduser('~/.elm'))
     another_elm_home_dir = os.path.join(elm_home_dir, 'another')
@@ -61,8 +87,9 @@ def main():
         elm_versions = os.listdir(another_elm_home_dir)
     except FileNotFoundError:
         print("""
-        Warning: nothing reset as elm home is empty (just run `another-elm`)
-        """.strip())
+        Warning: nothing reset as elm home is empty (just run `another-elm`).
+        """.strip(),
+              file=sys.stderr)
 
         return 0
 
@@ -79,8 +106,19 @@ def main():
 
     if not some_packages_reset:
         print("""
-        Warning: nothing reset as source is no newer than linked packages.)
-        """.strip())
+        Warning: nothing reset as source is no newer than linked packages.
+        """.strip(),
+              file=sys.stderr)
+
+    print("Success!", end='')
+    if binary_path.exists():
+        print('Reinstalled another-elm to "{}" and reset std packages.'.format(
+            binary_path))
+    else:
+        print('Installed another-elm to "{}".'.format(binary_path))
+
+    print('Note: Moving "{}" will break another-elm'.format(elm_std_dir))
+    print('      (run ./init.py again to fix)')
 
     return 0
 

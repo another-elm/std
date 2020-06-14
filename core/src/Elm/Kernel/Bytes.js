af9d89b77bb0b5d7d5a4a26905dc031af8121d71
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-06-14 21:37:49 +0100

    xo autofixes

--- core/src/Elm/Kernel/Bytes.js
+++ core/src/Elm/Kernel/Bytes.js
@@ -9,177 +9,213 @@ import Maybe exposing (Just, Nothing)
 
 // BYTES
 
-function _Bytes_width(bytes)
-{
-	return bytes.byteLength;
+function _Bytes_width(bytes) {
+  return bytes.byteLength;
 }
 
-var _Bytes_getHostEndianness = F2(function(le, be)
-{
-	return __Scheduler_binding(function(callback)
-	{
-		callback(__Scheduler_succeed(new Uint8Array(new Uint32Array([1]))[0] === 1 ? le : be));
-	});
+const _Bytes_getHostEndianness = F2(function (le, be) {
+  return __Scheduler_binding(function (callback) {
+    callback(__Scheduler_succeed(new Uint8Array(new Uint32Array([1]))[0] === 1 ? le : be));
+  });
 });
-
 
 // ENCODERS
 
-function _Bytes_encode(encoder)
-{
-	var mutableBytes = new DataView(new ArrayBuffer(__Encode_getWidth(encoder)));
-	__Encode_write(encoder)(mutableBytes)(0);
-	return mutableBytes;
+function _Bytes_encode(encoder) {
+  const mutableBytes = new DataView(new ArrayBuffer(__Encode_getWidth(encoder)));
+  __Encode_write(encoder)(mutableBytes)(0);
+  return mutableBytes;
 }
-
 
 // SIGNED INTEGERS
 
-var _Bytes_write_i8  = F3(function(mb, i, n) { mb.setInt8(i, n); return i + 1; });
-var _Bytes_write_i16 = F4(function(mb, i, n, isLE) { mb.setInt16(i, n, isLE); return i + 2; });
-var _Bytes_write_i32 = F4(function(mb, i, n, isLE) { mb.setInt32(i, n, isLE); return i + 4; });
-
+const _Bytes_write_i8 = F3(function (mb, i, n) {
+  mb.setInt8(i, n);
+  return i + 1;
+});
+const _Bytes_write_i16 = F4(function (mb, i, n, isLE) {
+  mb.setInt16(i, n, isLE);
+  return i + 2;
+});
+const _Bytes_write_i32 = F4(function (mb, i, n, isLE) {
+  mb.setInt32(i, n, isLE);
+  return i + 4;
+});
 
 // UNSIGNED INTEGERS
 
-var _Bytes_write_u8  = F3(function(mb, i, n) { mb.setUint8(i, n); return i + 1 ;});
-var _Bytes_write_u16 = F4(function(mb, i, n, isLE) { mb.setUint16(i, n, isLE); return i + 2; });
-var _Bytes_write_u32 = F4(function(mb, i, n, isLE) { mb.setUint32(i, n, isLE); return i + 4; });
-
+const _Bytes_write_u8 = F3(function (mb, i, n) {
+  mb.setUint8(i, n);
+  return i + 1;
+});
+const _Bytes_write_u16 = F4(function (mb, i, n, isLE) {
+  mb.setUint16(i, n, isLE);
+  return i + 2;
+});
+const _Bytes_write_u32 = F4(function (mb, i, n, isLE) {
+  mb.setUint32(i, n, isLE);
+  return i + 4;
+});
 
 // FLOATS
 
-var _Bytes_write_f32 = F4(function(mb, i, n, isLE) { mb.setFloat32(i, n, isLE); return i + 4; });
-var _Bytes_write_f64 = F4(function(mb, i, n, isLE) { mb.setFloat64(i, n, isLE); return i + 8; });
-
+const _Bytes_write_f32 = F4(function (mb, i, n, isLE) {
+  mb.setFloat32(i, n, isLE);
+  return i + 4;
+});
+const _Bytes_write_f64 = F4(function (mb, i, n, isLE) {
+  mb.setFloat64(i, n, isLE);
+  return i + 8;
+});
 
 // BYTES
 
-var _Bytes_write_bytes = F3(function(mb, offset, bytes)
-{
-	for (var i = 0, len = bytes.byteLength, limit = len - 4; i <= limit; i += 4)
-	{
-		mb.setUint32(offset + i, bytes.getUint32(i));
-	}
-	for (; i < len; i++)
-	{
-		mb.setUint8(offset + i, bytes.getUint8(i));
-	}
-	return offset + len;
-});
+const _Bytes_write_bytes = F3(function (mb, offset, bytes) {
+  for (var i = 0, length = bytes.byteLength, limit = length - 4; i <= limit; i += 4) {
+    mb.setUint32(offset + i, bytes.getUint32(i));
+  }
 
+  for (; i < length; i++) {
+    mb.setUint8(offset + i, bytes.getUint8(i));
+  }
+
+  return offset + length;
+});
 
 // STRINGS
 
-function _Bytes_getStringWidth(string)
-{
-	for (var width = 0, i = 0; i < string.length; i++)
-	{
-		var code = string.charCodeAt(i);
-		width +=
-			(code < 0x80) ? 1 :
-			(code < 0x800) ? 2 :
-			(code < 0xD800 || 0xDBFF < code) ? 3 : (i++, 4);
-	}
-	return width;
+function _Bytes_getStringWidth(string) {
+  for (var width = 0, i = 0; i < string.length; i++) {
+    const code = string.charCodeAt(i);
+    width += code < 0x80 ? 1 : code < 0x800 ? 2 : code < 0xd800 || code > 0xdbff ? 3 : (i++, 4);
+  }
+
+  return width;
 }
 
-var _Bytes_write_string = F3(function(mb, offset, string)
-{
-	for (var i = 0; i < string.length; i++)
-	{
-		var code = string.charCodeAt(i);
-		offset +=
-			(code < 0x80)
-				? (mb.setUint8(offset, code)
-				, 1
-				)
-				:
-			(code < 0x800)
-				? (mb.setUint16(offset, 0xC080 /* 0b1100000010000000 */
-					| (code >>> 6 & 0x1F /* 0b00011111 */) << 8
-					| code & 0x3F /* 0b00111111 */)
-				, 2
-				)
-				:
-			(code < 0xD800 || 0xDBFF < code)
-				? (mb.setUint16(offset, 0xE080 /* 0b1110000010000000 */
-					| (code >>> 12 & 0xF /* 0b00001111 */) << 8
-					| code >>> 6 & 0x3F /* 0b00111111 */)
-				, mb.setUint8(offset + 2, 0x80 /* 0b10000000 */
-					| code & 0x3F /* 0b00111111 */)
-				, 3
-				)
-				:
-			(code = (code - 0xD800) * 0x400 + string.charCodeAt(++i) - 0xDC00 + 0x10000
-			, mb.setUint32(offset, 0xF0808080 /* 0b11110000100000001000000010000000 */
-				| (code >>> 18 & 0x7 /* 0b00000111 */) << 24
-				| (code >>> 12 & 0x3F /* 0b00111111 */) << 16
-				| (code >>> 6 & 0x3F /* 0b00111111 */) << 8
-				| code & 0x3F /* 0b00111111 */)
-			, 4
-			);
-	}
-	return offset;
-});
+const _Bytes_write_string = F3(function (mb, offset, string) {
+  for (let i = 0; i < string.length; i++) {
+    let code = string.charCodeAt(i);
+    offset +=
+      code < 0x80
+        ? (mb.setUint8(offset, code), 1)
+        : code < 0x800
+        ? (mb.setUint16(
+            offset,
+            0xc080 /* 0b1100000010000000 */ |
+              (((code >>> 6) & 0x1f) /* 0b00011111 */ << 8) |
+              (code & 0x3f) /* 0b00111111 */
+          ),
+          2)
+        : code < 0xd800 || code > 0xdbff
+        ? (mb.setUint16(
+            offset,
+            0xe080 /* 0b1110000010000000 */ |
+              (((code >>> 12) & 0xf) /* 0b00001111 */ << 8) |
+              ((code >>> 6) & 0x3f) /* 0b00111111 */
+          ),
+          mb.setUint8(offset + 2, 0x80 /* 0b10000000 */ | (code & 0x3f) /* 0b00111111 */),
+          3)
+        : ((code = (code - 0xd800) * 0x400 + string.charCodeAt(++i) - 0xdc00 + 0x10000),
+          mb.setUint32(
+            offset,
+            0xf0808080 /* 0b11110000100000001000000010000000 */ |
+              (((code >>> 18) & 0x7) /* 0b00000111 */ << 24) |
+              (((code >>> 12) & 0x3f) /* 0b00111111 */ << 16) |
+              (((code >>> 6) & 0x3f) /* 0b00111111 */ << 8) |
+              (code & 0x3f) /* 0b00111111 */
+          ),
+          4);
+  }
 
+  return offset;
+});
 
 // DECODER
 
-var _Bytes_decode = F2(function(decoder, bytes)
-{
-	try {
-		return __Maybe_Just(A2(decoder, bytes, 0).b);
-	} catch(e) {
-		return __Maybe_Nothing;
-	}
+const _Bytes_decode = F2(function (decoder, bytes) {
+  try {
+    return __Maybe_Just(A2(decoder, bytes, 0).b);
+  } catch (error) {
+    return __Maybe_Nothing;
+  }
 });
 
-var _Bytes_read_i8  = F2(function(      bytes, offset) { return __Utils_Tuple2(offset + 1, bytes.getInt8(offset)); });
-var _Bytes_read_i16 = F3(function(isLE, bytes, offset) { return __Utils_Tuple2(offset + 2, bytes.getInt16(offset, isLE)); });
-var _Bytes_read_i32 = F3(function(isLE, bytes, offset) { return __Utils_Tuple2(offset + 4, bytes.getInt32(offset, isLE)); });
-var _Bytes_read_u8  = F2(function(      bytes, offset) { return __Utils_Tuple2(offset + 1, bytes.getUint8(offset)); });
-var _Bytes_read_u16 = F3(function(isLE, bytes, offset) { return __Utils_Tuple2(offset + 2, bytes.getUint16(offset, isLE)); });
-var _Bytes_read_u32 = F3(function(isLE, bytes, offset) { return __Utils_Tuple2(offset + 4, bytes.getUint32(offset, isLE)); });
-var _Bytes_read_f32 = F3(function(isLE, bytes, offset) { return __Utils_Tuple2(offset + 4, bytes.getFloat32(offset, isLE)); });
-var _Bytes_read_f64 = F3(function(isLE, bytes, offset) { return __Utils_Tuple2(offset + 8, bytes.getFloat64(offset, isLE)); });
-
-var _Bytes_read_bytes = F3(function(len, bytes, offset)
-{
-	return __Utils_Tuple2(offset + len, new DataView(bytes.buffer, bytes.byteOffset + offset, len));
+const _Bytes_read_i8 = F2(function (bytes, offset) {
+  return __Utils_Tuple2(offset + 1, bytes.getInt8(offset));
+});
+const _Bytes_read_i16 = F3(function (isLE, bytes, offset) {
+  return __Utils_Tuple2(offset + 2, bytes.getInt16(offset, isLE));
+});
+const _Bytes_read_i32 = F3(function (isLE, bytes, offset) {
+  return __Utils_Tuple2(offset + 4, bytes.getInt32(offset, isLE));
+});
+const _Bytes_read_u8 = F2(function (bytes, offset) {
+  return __Utils_Tuple2(offset + 1, bytes.getUint8(offset));
+});
+const _Bytes_read_u16 = F3(function (isLE, bytes, offset) {
+  return __Utils_Tuple2(offset + 2, bytes.getUint16(offset, isLE));
+});
+const _Bytes_read_u32 = F3(function (isLE, bytes, offset) {
+  return __Utils_Tuple2(offset + 4, bytes.getUint32(offset, isLE));
+});
+const _Bytes_read_f32 = F3(function (isLE, bytes, offset) {
+  return __Utils_Tuple2(offset + 4, bytes.getFloat32(offset, isLE));
+});
+const _Bytes_read_f64 = F3(function (isLE, bytes, offset) {
+  return __Utils_Tuple2(offset + 8, bytes.getFloat64(offset, isLE));
 });
 
-var _Bytes_read_string = F3(function(len, bytes, offset)
-{
-	var string = '';
-	var end = offset + len;
-	for (; offset < end;)
-	{
-		var byte = bytes.getUint8(offset++);
-		string +=
-			(byte < 128)
-				? String.fromCharCode(byte)
-				:
-			((byte & 0xE0 /* 0b11100000 */) === 0xC0 /* 0b11000000 */)
-				? String.fromCharCode((byte & 0x1F /* 0b00011111 */) << 6 | bytes.getUint8(offset++) & 0x3F /* 0b00111111 */)
-				:
-			((byte & 0xF0 /* 0b11110000 */) === 0xE0 /* 0b11100000 */)
-				? String.fromCharCode(
-					(byte & 0xF /* 0b00001111 */) << 12
-					| (bytes.getUint8(offset++) & 0x3F /* 0b00111111 */) << 6
-					| bytes.getUint8(offset++) & 0x3F /* 0b00111111 */
-				)
-				:
-				(byte =
-					((byte & 0x7 /* 0b00000111 */) << 18
-						| (bytes.getUint8(offset++) & 0x3F /* 0b00111111 */) << 12
-						| (bytes.getUint8(offset++) & 0x3F /* 0b00111111 */) << 6
-						| bytes.getUint8(offset++) & 0x3F /* 0b00111111 */
-					) - 0x10000
-				, String.fromCharCode(Math.floor(byte / 0x400) + 0xD800, byte % 0x400 + 0xDC00)
-				);
-	}
-	return __Utils_Tuple2(offset, string);
+const _Bytes_read_bytes = F3(function (length, bytes, offset) {
+  return __Utils_Tuple2(
+    offset + length,
+    new DataView(bytes.buffer, bytes.byteOffset + offset, length)
+  );
 });
 
-var _Bytes_decodeFailure = F2(function() { throw 0; });
+const _Bytes_read_string = F3(function (length, bytes, offset) {
+  let string = "";
+  const end = offset + length;
+  for (; offset < end; ) {
+    let byte = bytes.getUint8(offset++);
+    string +=
+      byte < 128
+        ? String.fromCharCode(byte)
+        : (byte & 0xe0) /* 0b11100000 */ === 0xc0 /* 0b11000000 */
+        ? String.fromCharCode(
+            ((byte & 0x1f) /* 0b00011111 */ << 6) |
+              (bytes.getUint8(offset++) & 0x3f) /* 0b00111111 */
+          )
+        : (byte & 0xf0) /* 0b11110000 */ === 0xe0 /* 0b11100000 */
+        ? String.fromCharCode(
+            ((byte & 0xf) /* 0b00001111 */ << 12) |
+              ((bytes.getUint8(offset++) & 0x3f) /* 0b00111111 */ << 6) |
+              (bytes.getUint8(offset++) & 0x3f) /* 0b00111111 */
+          )
+        : ((byte =
+            (((byte & 0x7) /* 0b00000111 */ << 18) |
+              ((bytes.getUint8(offset++) & 0x3f) /* 0b00111111 */ << 12) |
+              ((bytes.getUint8(offset++) & 0x3f) /* 0b00111111 */ << 6) |
+              (bytes.getUint8(offset++) & 0x3f)) /* 0b00111111 */ -
+            0x10000),
+          String.fromCharCode(Math.floor(byte / 0x400) + 0xd800, (byte % 0x400) + 0xdc00));
+  }
+
+  return __Utils_Tuple2(offset, string);
+});
+
+const _Bytes_decodeFailure = F2(function () {
+  throw 0;
+});
+
+/* ESLINT GLOBAL VARIABLES
+ *
+ * Do not edit below this line as it is generated by tests/generate-globals.py
+ */
+
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "_Bytes_.*" }] */
+
+/* global __Encode_getWidth, __Encode_write */
+/* global __Scheduler_binding, __Scheduler_succeed */
+/* global __Utils_Tuple2, __Utils_chr */
+/* global __Maybe_Just, __Maybe_Nothing */
--- core/src/Elm/Kernel/Http.js
+++ core/src/Elm/Kernel/Http.js
@@ -10,177 +10,217 @@ import Result exposing (map, isOk)
 
 */
 
-
 // SEND REQUEST
 
-var _Http_toTask = F3(function(router, toTask, request)
-{
-	return __Scheduler_binding(function(callback)
-	{
-		function done(response) {
-			callback(toTask(request.__$expect.__toValue(response)));
-		}
+const _Http_toTask = F3(function (router, toTask, request) {
+  return __Scheduler_binding(function (callback) {
+    function done(response) {
+      callback(toTask(request.__$expect.__toValue(response)));
+    }
 
-		var xhr = new XMLHttpRequest();
-		xhr.addEventListener('error', function() { done(__Http_NetworkError_); });
-		xhr.addEventListener('timeout', function() { done(__Http_Timeout_); });
-		xhr.addEventListener('load', function() { done(_Http_toResponse(request.__$expect.__toBody, xhr)); });
-		__Maybe_isJust(request.__$tracker) && _Http_track(router, xhr, request.__$tracker.a);
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("error", function () {
+      done(__Http_NetworkError_);
+    });
+    xhr.addEventListener("timeout", function () {
+      done(__Http_Timeout_);
+    });
+    xhr.addEventListener("load", function () {
+      done(_Http_toResponse(request.__$expect.__toBody, xhr));
+    });
+    __Maybe_isJust(request.__$tracker) && _Http_track(router, xhr, request.__$tracker.a);
 
-		try {
-			xhr.open(request.__$method, request.__$url, true);
-		} catch (e) {
-			return done(__Http_BadUrl_(request.__$url));
-		}
+    try {
+      xhr.open(request.__$method, request.__$url, true);
+    } catch (error) {
+      return done(__Http_BadUrl_(request.__$url));
+    }
 
-		_Http_configureRequest(xhr, request);
+    _Http_configureRequest(xhr, request);
 
-		request.__$body.a && xhr.setRequestHeader('Content-Type', request.__$body.a);
-		xhr.send(request.__$body.b);
+    request.__$body.a && xhr.setRequestHeader("Content-Type", request.__$body.a);
+    xhr.send(request.__$body.b);
 
-		return function() { xhr.__isAborted = true; xhr.abort(); };
-	});
+    return function () {
+      xhr.__isAborted = true;
+      xhr.abort();
+    };
+  });
 });
-
 
 // CONFIGURE
 
-function _Http_configureRequest(xhr, request)
-{
-	for (var headers = request.__$headers; headers.b; headers = headers.b) // WHILE_CONS
-	{
-		xhr.setRequestHeader(headers.a.a, headers.a.b);
-	}
-	xhr.timeout = request.__$timeout.a || 0;
-	xhr.responseType = request.__$expect.__type;
-	xhr.withCredentials = request.__$allowCookiesFromOtherDomains;
-}
+function _Http_configureRequest(xhr, request) {
+  for (
+    let headers = request.__$headers;
+    headers.b;
+    headers = headers.b // WHILE_CONS
+  ) {
+    xhr.setRequestHeader(headers.a.a, headers.a.b);
+  }
 
+  xhr.timeout = request.__$timeout.a || 0;
+  xhr.responseType = request.__$expect.__type;
+  xhr.withCredentials = request.__$allowCookiesFromOtherDomains;
+}
 
 // RESPONSES
 
-function _Http_toResponse(toBody, xhr)
-{
-	return A2(
-		200 <= xhr.status && xhr.status < 300 ? __Http_GoodStatus_ : __Http_BadStatus_,
-		_Http_toMetadata(xhr),
-		toBody(xhr.response)
-	);
+function _Http_toResponse(toBody, xhr) {
+  return A2(
+    xhr.status >= 200 && xhr.status < 300 ? __Http_GoodStatus_ : __Http_BadStatus_,
+    _Http_toMetadata(xhr),
+    toBody(xhr.response)
+  );
 }
-
 
 // METADATA
 
-function _Http_toMetadata(xhr)
-{
-	return {
-		__$url: xhr.responseURL,
-		__$statusCode: xhr.status,
-		__$statusText: xhr.statusText,
-		__$headers: _Http_parseHeaders(xhr.getAllResponseHeaders())
-	};
+function _Http_toMetadata(xhr) {
+  return {
+    __$url: xhr.responseURL,
+    __$statusCode: xhr.status,
+    __$statusText: xhr.statusText,
+    __$headers: _Http_parseHeaders(xhr.getAllResponseHeaders()),
+  };
 }
-
 
 // HEADERS
 
-function _Http_parseHeaders(rawHeaders)
-{
-	if (!rawHeaders)
-	{
-		return __Dict_empty;
-	}
+function _Http_parseHeaders(rawHeaders) {
+  if (!rawHeaders) {
+    return __Dict_empty;
+  }
 
-	var headers = __Dict_empty;
-	var headerPairs = rawHeaders.split('\r\n');
-	for (var i = headerPairs.length; i--; )
-	{
-		var headerPair = headerPairs[i];
-		var index = headerPair.indexOf(': ');
-		if (index > 0)
-		{
-			var key = headerPair.substring(0, index);
-			var value = headerPair.substring(index + 2);
+  let headers = __Dict_empty;
+  const headerPairs = rawHeaders.split("\r\n");
+  for (let i = headerPairs.length; i--; ) {
+    const headerPair = headerPairs[i];
+    const index = headerPair.indexOf(": ");
+    if (index > 0) {
+      const key = headerPair.slice(0, Math.max(0, index));
+      var value = headerPair.slice(Math.max(0, index + 2));
 
-			headers = A3(__Dict_update, key, function(oldValue) {
-				return __Maybe_Just(__Maybe_isJust(oldValue)
-					? value + ', ' + oldValue.a
-					: value
-				);
-			}, headers);
-		}
-	}
-	return headers;
+      headers = A3(
+        __Dict_update,
+        key,
+        function (oldValue) {
+          return __Maybe_Just(__Maybe_isJust(oldValue) ? value + ", " + oldValue.a : value);
+        },
+        headers
+      );
+    }
+  }
+
+  return headers;
 }
-
 
 // EXPECT
 
-var _Http_expect = F3(function(type, toBody, toValue)
-{
-	return {
-		$: 0,
-		__type: type,
-		__toBody: toBody,
-		__toValue: toValue
-	};
+const _Http_expect = F3(function (type, toBody, toValue) {
+  return {
+    $: 0,
+    __type: type,
+    __toBody: toBody,
+    __toValue: toValue,
+  };
 });
 
-var _Http_mapExpect = F2(function(func, expect)
-{
-	return {
-		$: 0,
-		__type: expect.__type,
-		__toBody: expect.__toBody,
-		__toValue: function(x) { return func(expect.__toValue(x)); }
-	};
+const _Http_mapExpect = F2(function (func, expect) {
+  return {
+    $: 0,
+    __type: expect.__type,
+    __toBody: expect.__toBody,
+    __toValue(x) {
+      return func(expect.__toValue(x));
+    },
+  };
 });
 
-function _Http_toDataView(arrayBuffer)
-{
-	return new DataView(arrayBuffer);
+function _Http_toDataView(arrayBuffer) {
+  return new DataView(arrayBuffer);
 }
-
 
 // BODY and PARTS
 
-var _Http_emptyBody = { $: 0 };
-var _Http_pair = F2(function(a, b) { return { $: 0, a: a, b: b }; });
-
-function _Http_toFormData(parts)
-{
-	for (var formData = new FormData(); parts.b; parts = parts.b) // WHILE_CONS
-	{
-		var part = parts.a;
-		formData.append(part.a, part.b);
-	}
-	return formData;
-}
-
-var _Http_bytesToBlob = F2(function(mime, bytes)
-{
-	return new Blob([bytes], { type: mime });
+const _Http_emptyBody = { $: 0 };
+const _Http_pair = F2(function (a, b) {
+  return { $: 0, a, b };
 });
 
+function _Http_toFormData(parts) {
+  for (
+    var formData = new FormData();
+    parts.b;
+    parts = parts.b // WHILE_CONS
+  ) {
+    const part = parts.a;
+    formData.append(part.a, part.b);
+  }
+
+  return formData;
+}
+
+const _Http_bytesToBlob = F2(function (mime, bytes) {
+  return new Blob([bytes], { type: mime });
+});
 
 // PROGRESS
 
-function _Http_track(router, xhr, tracker)
-{
-	// TODO check out lengthComputable on loadstart event
+function _Http_track(router, xhr, tracker) {
+  // TODO check out lengthComputable on loadstart event
 
-	xhr.upload.addEventListener('progress', function(event) {
-		if (xhr.__isAborted) { return; }
-		__Scheduler_rawSpawn(A2(__Platform_sendToSelf, router, __Utils_Tuple2(tracker, __Http_Sending({
-			__$sent: event.loaded,
-			__$size: event.total
-		}))));
-	});
-	xhr.addEventListener('progress', function(event) {
-		if (xhr.__isAborted) { return; }
-		__Scheduler_rawSpawn(A2(__Platform_sendToSelf, router, __Utils_Tuple2(tracker, __Http_Receiving({
-			__$received: event.loaded,
-			__$size: event.lengthComputable ? __Maybe_Just(event.total) : __Maybe_Nothing
-		}))));
-	});
+  xhr.upload.addEventListener("progress", function (event) {
+    if (xhr.__isAborted) {
+      return;
+    }
+
+    __Scheduler_rawSpawn(
+      A2(
+        __Platform_sendToSelf,
+        router,
+        __Utils_Tuple2(
+          tracker,
+          __Http_Sending({
+            __$sent: event.loaded,
+            __$size: event.total,
+          })
+        )
+      )
+    );
+  });
+  xhr.addEventListener("progress", function (event) {
+    if (xhr.__isAborted) {
+      return;
+    }
+
+    __Scheduler_rawSpawn(
+      A2(
+        __Platform_sendToSelf,
+        router,
+        __Utils_Tuple2(
+          tracker,
+          __Http_Receiving({
+            __$received: event.loaded,
+            __$size: event.lengthComputable ? __Maybe_Just(event.total) : __Maybe_Nothing,
+          })
+        )
+      )
+    );
+  });
 }
+
+/* ESLINT GLOBAL VARIABLES
+ *
+ * Do not edit below this line as it is generated by tests/generate-globals.py
+ */
+
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "_Http_.*" }] */
+
+/* global __Dict_empty, __Dict_update */
+/* global __Scheduler_binding, __Scheduler_fail, __Scheduler_rawSpawn, __Scheduler_succeed */
+/* global __Utils_Tuple2 */
+/* global __Http_BadUrl_, __Http_Timeout_, __Http_NetworkError_, __Http_BadStatus_, __Http_GoodStatus_, __Http_Sending, __Http_Receiving */
+/* global __Maybe_Just, __Maybe_Nothing, __Maybe_isJust */
+/* global __Platform_sendToApp, __Platform_sendToSelf */
+/* global __Result_map, __Result_isOk */

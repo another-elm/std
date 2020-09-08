fff35c50743e6fa672abbf7fb14efe9ef25270c3
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-09-08 17:17:24 +0100

    run lint on more kernel files

--- core/src/Elm/Kernel/Char.js
+++ core/src/Elm/Kernel/Char.js
@@ -4,25 +4,12 @@ import Elm.Kernel.Utils exposing (chr)
 
 */
 
-/* eslint-disable */
-
 function _Char_toCode(char) {
-  const code = char.charCodeAt(0);
-  if (0xd800 <= code && code <= 0xdbff) {
-    return (code - 0xd800) * 0x400 + char.charCodeAt(1) - 0xdc00 + 0x10000;
-  }
-  return code;
+  return char.codePointAt(0);
 }
 
 function _Char_fromCode(code) {
-  return __Utils_chr(
-    code < 0 || 0x10ffff < code
-      ? "\uFFFD"
-      : code <= 0xffff
-      ? String.fromCharCode(code)
-      : ((code -= 0x10000),
-        String.fromCharCode(Math.floor(code / 0x400) + 0xd800, (code % 0x400) + 0xdc00))
-  );
+  return __Utils_chr(code < 0 || code > 0x10ffff ? "\uFFFD" : String.fromCodePoint(code));
 }
 
 function _Char_toUpper(char) {
